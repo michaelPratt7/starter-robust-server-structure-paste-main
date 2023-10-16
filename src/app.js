8468fb2fa0fe6031a0846d29fe4a115361914532
Michael Pratt <mrpratt7751@gmail.com>
2023-10-15 20:14:25 -0400

    deploy

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const pastes = require("./data/pastes-data");
+const pastesRouter = require("./pastes/pastes.router");
+const usersRouter = require("./users/users.router");
+
+// TODO: Follow instructions in the checkpoint to implement ths API.
+
+app.use(express.json());
+
+app.use("/pastes", pastesRouter);
+app.use("/users", usersRouter);
+
+
+// Not found handler
+app.use((request, response, next) => {
+  next(`Not found: ${request.originalUrl}`);
+});
+
+// Error handler
+app.use((error, request, response, next) => {
+  console.error(error);
+  const {status = 500, message = "Something went wrong!"} = error;
+  response.status(status).json({error: message})
+});
+
+module.exports = app;
